264928489351577f62b9856f25f8f1fd94a1ad6c
Nguyễn Tiến Long <email ng k git>
2021-12-17 12:08:36 +0700

    bt

--- templates/js/logout.js
+++ templates/js/logout.js
@@ -3,5 +3,5 @@ document.getElementById("logout__button").addEventListener("click", logout);
 function logout(){
     event.preventDefault;
     localStorage.removeItem("data")
-    window.location.href="/login-logout/login.html"
+    window.location.href="../login-logout/login.html"
 }
