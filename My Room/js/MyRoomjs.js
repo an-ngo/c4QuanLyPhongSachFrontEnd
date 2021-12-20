a2a488968c55ace8c25a2ae6cb16a9cfd8ec935d
cuongtien00 <cuonghiep000@gmail.com>
2021-12-20 08:30:14 +0700

    add tinh nag

--- My Room/js/MyRoomjs.js
+++ My Room/js/MyRoomjs.js
@@ -14,7 +14,7 @@ function getRoom(data){
                     <div class="u-price u-text-palette-2-base" style="font-size: 1.5rem; font-weight: 700;"><!--product_regular_price_content--><!--/product_regular_price_content--></div><!--/product_regular_price-->
                   </div>
                 </div><!--/product_price--><!--product_button--><!--options_json--><!--{"clickType":"add-to-cart","content":"buy it now"}--><!--/options_json-->
-                <a href="https://nicepage.one" class="u-border-2 u-border-palette-2-base u-btn u-button-style u-hover-black u-palette-2-base u-product-control u-text-black u-text-hover-white u-btn-1"><!--product_button_content-->JOIN ROOM<!--/product_button_content--></a><!--/product_button--><!--product_button--><!--options_json--><!--{"clickType":"add-to-cart","content":"COme IN"}--><!--/options_json-->
+                <a onclick="removeRoom(this)" id="${data.id}" class="u-border-2 u-border-palette-2-base u-btn u-button-style u-hover-black u-palette-2-base u-product-control u-text-black u-text-hover-white u-btn-1"><!--product_button_content-->Sell<!--/product_button_content--></a><!--/product_button--><!--product_button--><!--options_json--><!--{"clickType":"add-to-cart","content":"COme IN"}--><!--/options_json-->
                 <a id="${data.id}" onclick="openViewRoom(this)" class="u-border-none u-btn u-button-style u-hover-palette-1-dark-1 u-palette-5-light-3 u-product-control u-btn-2" >COme IN</a><!--/product_button-->
                 <a href="https://nicepage.com/k/astrology-website-templates" class="u-btn u-button-style u-none u-text-palette-2-base u-btn-3">Full Detail&nbsp;<span class="u-icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 512 512" x="0px" y="0px" style="width: 1em; height: 1em;"><g><g><path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068    c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557    l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104    c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z"></path>
 </g>
@@ -36,6 +36,20 @@ function getRoom(data){
     </section>`
 }
 
+function removeRoom(a){
+    let roomId = a.getAttribute("id");
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
+            'Access-control-allow-origin': '*'
+        },
+        type: "DELETE",
+        url: "http://localhost:8087/rooms/" + roomId,
+        success:myRoom
+    })
+}
 
 function myRoom() {
     let data = JSON.parse(localStorage.getItem("data"));
--- templates/js/getDataOnLoad.js
+++ templates/js/getDataOnLoad.js
@@ -9,8 +9,10 @@ function getNameAndMoney(){
     type: "GET",
     url: "http://localhost:8087/customers/" + JSON.parse(localStorage.getItem("data")).idCustomer,
     success: function (data) {
+          let content = '<img src="../../customer/img/'+data.avatar+'" style="width: 100%;height: 100%;border-radius: 100px;border: 1px solid white; alt=""/>'
       document.getElementById("money__button").innerHTML = `$${data.money}.00`;
       document.getElementById("name__button").innerHTML = `${data.name}`;
+      document.getElementById("image__button").innerHTML = content;
 
     },
     })
