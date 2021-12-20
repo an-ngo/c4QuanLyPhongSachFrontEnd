56a095d0cd2cd402bccffb5c4cf75f23dbe812a1
Nguyễn Tiến Long <email ng k git>
2021-12-20 23:41:12 +0700

    bt

--- bookshelf/booksJs/booksjs.js
+++ bookshelf/booksJs/booksjs.js
@@ -7,68 +7,75 @@ function findAllBook() {
     let id = data.idCustomer;
     $.ajax({
 
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer '+JSON.parse(localStorage.getItem("data")).jwttoken,
-    'Access-control-allow-origin':'*'
-},
-    type:"GET",
-    url:"http://localhost:8087/books/customer/"+id,
-    success:function (data){
-    for (let i = 0; i < data.length; i++) {
-    content +=
-    '<div style="display:flex;border: 1px solid black;align-items: center;padding: 10px;border-radius: 10px;margin-bottom: 10px">\n' +
-    '                                <div id="leftSidebar" style="width: 938px;text-align: left">\n' +
-    '                                    <h3>'+data[i].name+'</h3>\n' +
-    '                                    <h5 style="font-weight:unset" >'+data[i].type.name+'</h5>\n' +
-    '                                </div>\n' +
-    '                                <div id="rightSidebar">\n' +
-    '                                    <button value="'+data[i].id+'" onclick="putBookIntoBookshelf(this)" class="btn btn-primary">Cất vào kệ</button>\n' +
-    '                                </div>\n' +
-    '                            </div>';
-    }
-    content+='<div id="alert__location"></div>'
-    document.getElementById("modal-body").innerHTML = content;
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
+            'Access-control-allow-origin': '*'
+        },
+        type: "GET",
+        url: "http://localhost:8087/books/customer/" + id,
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                content +=
+                    '<div style="display:flex;border: 1px solid black;align-items: center;padding: 10px;border-radius: 10px;margin-bottom: 10px">\n' +
+                    '                                <div id="leftSidebar" style="width: 938px;text-align: left">\n' +
+                    '                                    <h3>' + data[i].name + '</h3>\n' +
+                    '                                    <h5 style="font-weight:unset" >' + data[i].type.name + '</h5>\n' +
+                    '                                </div>\n' +
+                    '                                <div id="rightSidebar">\n' +
+                    '                                    <button value="' + data[i].id + '" onclick="putBookIntoBookshelf(this)" class="btn btn-primary">Cất vào kệ</button>\n' +
+                    '                                </div>\n' +
+                    '                            </div>';
+            }
+            content += '<div id="alert__location"></div>'
+            document.getElementById("modal-body").innerHTML = content;
+        }
+    });
 }
-});
-}
-    function putBookIntoBookshelf(a){
+
+function putBookIntoBookshelf(a) {
     let bookShelfId = $("#bookshelfId").val();
     let data = JSON.parse(localStorage.getItem("data"));
     let customerId = data.idCustomer;
     let bookId = a.getAttribute("value");
     $.ajax({
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
-    'Access-control-allow-origin': '*'
-},
-    type:"PUT",
-    url:"http://localhost:8087/books/"+customerId+"/"+bookShelfId+"/"+bookId,
-    success:function (){
-        notification1()
-        findAllBook();
-    showAllBookByLocationBookIdAndCustomerId();
-},
-    error: function (){
-        document.getElementById("alert__location").innerHTML = '<div class="alert alert-danger" role="alert">\n' +
-            '  <h4 class="alert-heading">Có Lỗi Xảy Ra!</h4>\n' +
-            '  <p> Oops. Có vẻ như kệ đã hết chỗ để sách. Bạn phải lấy một vài quyển sách ra trước khi thêm lại sách.</p>\n' +
-            '  <hr>\n' +
-            '  <p class="mb-0">Whenever you need to do, books always by your side.</p>\n' +
-            '</div>';
-        setTimeout(function (){
-            document.getElementById("alert__location").innerHTML = '';
-        },4000)
-    // alert("Kệ sách đã đầy, vui lòng chọn kệ khác!");
-}
-});
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
+            'Access-control-allow-origin': '*'
+        },
+        type: "PUT",
+        url: "http://localhost:8087/books/" + customerId + "/" + bookShelfId + "/" + bookId,
+        success: function () {
+            notification1()
+            findAllBook();
+            showAllBookByLocationBookIdAndCustomerId();
+        },
+        error: function () {
+            document.getElementById("alert__location").innerHTML = '<div class="alert alert-danger" role="alert" style="position: fixed;' +
+                'right: 400px;' +
+                'bottom: 300px;' +
+                'z-index: 99;' +
+                'opacity: 1;' +
+                'transition: 1s;' +
+                'padding: 20px;">' +
+                '  <h4 class="alert-heading">Có Lỗi Xảy Ra!</h4>\n' +
+                '  <p> Oops. Có vẻ như kệ đã hết chỗ để sách. Bạn phải lấy một vài quyển sách ra trước khi thêm lại sách.</p>\n' +
+                '  <hr>\n' +
+                '  <p class="mb-0">Whenever you need to do, books always by your side.</p>\n' +
+                '</div>';
+            setTimeout(function () {
+                document.getElementById("alert__location").innerHTML = '';
+            }, 4000)
+            // alert("Kệ sách đã đầy, vui lòng chọn kệ khác!");
+        }
+    });
 }
 
-                         // thông báo cất sách
-function notification1(){
+// thông báo cất sách
+function notification1() {
     const myTimeout = setTimeout(closeThis, 4000);
     let content = `
                 <!-- Danger Alert Message -->
@@ -88,13 +95,14 @@ function notification1(){
 </div>`
     document.getElementById("note1").innerHTML = content
 }
-function closeThis(){
+
+function closeThis() {
     document.getElementById("cha").style.display = "none";
 }
 
 
 // thông bảo cất sách vào giỏ hàng
-function notification2(){
+function notification2() {
     const myTimeout = setTimeout(closeThis, 4000);
     let content = `
                 <!-- Danger Alert Message -->
@@ -114,7 +122,6 @@ function notification2(){
 </div>`
     document.getElementById("note2").innerHTML = content
 }
-
 
 
 function returnBookComeToCart(a) {
@@ -142,21 +149,21 @@ function returnBookComeToCart(a) {
 function showLocationBookId() {
     let locationBookId = JSON.parse(localStorage.getItem("bookshelfId"));
     $.ajax({
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
-    'Access-control-allow-origin': '*'
-},
-    type:"GET",
-    url:"http://localhost:8087/locationBooks/"+locationBookId,
-    success:function (data){
-    let content1 = '<input id="bookshelfId" type="hidden" value="'+data.id+'"/>'
-    document.getElementById("showBookshelfId").innerHTML = content1;
-    // localStorage.removeItem("bookshelfId");
-    showAllBookByLocationBookIdAndCustomerId()
-}
-})
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
+            'Access-control-allow-origin': '*'
+        },
+        type: "GET",
+        url: "http://localhost:8087/locationBooks/" + locationBookId,
+        success: function (data) {
+            let content1 = '<input id="bookshelfId" type="hidden" value="' + data.id + '"/>'
+            document.getElementById("showBookshelfId").innerHTML = content1;
+            // localStorage.removeItem("bookshelfId");
+            showAllBookByLocationBookIdAndCustomerId()
+        }
+    })
 }
 
 //list book cua bookShelf
@@ -165,76 +172,76 @@ function showAllBookByLocationBookIdAndCustomerId() {
     let data = JSON.parse(localStorage.getItem("data"));
     let customerId = data.idCustomer;
     $.ajax({
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
-    'Access-control-allow-origin': '*'
-},
-    type: "GET",
-    url: "http://localhost:8087/books/" + locationBookId + "/" + customerId,
-    success: function (data) {
-    let content = '';
-    for (let i = 0; i < data.length; i++) {
-    content += `<div style="border: 1px solid brown;width:379.99px; background-color: white" id="book-${i}">\n` +
-    '                <img style="margin-top: 20px" alt="" class="u-image u-image-default u-image-1" data-image-width="404" data-image-height="404" src="../customer/img/' + data[i].image + '">\n' +
-    // '                <img style="margin-top: 20px" alt="" class="u-image u-image-default u-image-1" data-image-width="404" data-image-height="404" src="'+'../customer/img'+data[i].image+'">\n' +
-    '                <h3 style="color: black" class="u-align-center u-custom-font u-font-oswald u-text u-text-2">' + data[i].name + '</h3>\n' +
-    '                <p class="u-align-center u-text u-text-palette-3-base u-text-3">' + data[i].type.name + '</p>\n' +
-    // '                <input type="hidden" id="bookId" value="'+data[i].id+'"></input>\n' +
-    '                <div style="display: flex">\n' +
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("data")).jwttoken,
+            'Access-control-allow-origin': '*'
+        },
+        type: "GET",
+        url: "http://localhost:8087/books/" + locationBookId + "/" + customerId,
+        success: function (data) {
+            let content = '';
+            for (let i = 0; i < data.length; i++) {
+                content += `<div style="border: 1px solid brown;width:379.99px; background-color: white" id="book-${i}">\n` +
+                    '                <img style="margin-top: 20px" alt="" class="u-image u-image-default u-image-1" data-image-width="404" data-image-height="404" src="../customer/img/' + data[i].image + '">\n' +
+                    // '                <img style="margin-top: 20px" alt="" class="u-image u-image-default u-image-1" data-image-width="404" data-image-height="404" src="'+'../customer/img'+data[i].image+'">\n' +
+                    '                <h3 style="color: black" class="u-align-center u-custom-font u-font-oswald u-text u-text-2">' + data[i].name + '</h3>\n' +
+                    '                <p class="u-align-center u-text u-text-palette-3-base u-text-3">' + data[i].type.name + '</p>\n' +
+                    // '                <input type="hidden" id="bookId" value="'+data[i].id+'"></input>\n' +
+                    '                <div style="display: flex">\n' +
 
-    // '                  <button  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" onclick="viewBook(this)" value="' + data[i].id + '" >view</button>\n' +
-    // '                  <button style="margin-right: 40px;"  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" value="' + data[i].id + '" onclick="returnBookComeToCart(this)" >remove</button>\n' +
+                    // '                  <button  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" onclick="viewBook(this)" value="' + data[i].id + '" >view</button>\n' +
+                    // '                  <button style="margin-right: 40px;"  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" value="' + data[i].id + '" onclick="returnBookComeToCart(this)" >remove</button>\n' +
 
-    '                  <button  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" onclick="viewBook(this)" value="' + data[i].id + '" data-bs-toggle="modal" data-bs-target="#exampleModal1">view</button>\n' +
-    '                  <button style="margin-right: 40px;"  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" value="' + data[i].id + '" onclick="returnBookComeToCart(this)" >remove</button>\n' +
+                    '                  <button  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" onclick="viewBook(this)" value="' + data[i].id + '" data-bs-toggle="modal" data-bs-target="#exampleModal1">view</button>\n' +
+                    '                  <button style="margin-right: 40px;"  class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1" value="' + data[i].id + '" onclick="returnBookComeToCart(this)" >remove</button>\n' +
 
-    '                </div>\n' +
+                    '                </div>\n' +
 
-        `<form><div id="rating-${i}" class="mt-3 mb-2">
-    <input type="radio" id="star5-${i}" name="rating" value="5" onclick="click_rate(this,`+data[i].id+`)" />
+                    `<form><div id="rating-${i}" class="mt-3 mb-2">
+    <input type="radio" id="star5-${i}" name="rating" value="5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class = "full" for="star5-${i}" title="Awesome - 5 stars"></label>
     
-    <input type="radio" id="star4half-${i}" name="rating" value="4.5" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star4half-${i}" name="rating" value="4.5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class="half" for="star4half-${i}" title="Pretty good - 4.5 stars"></label>
     
-    <input type="radio" id="star4-${i}" name="rating" value="4" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star4-${i}" name="rating" value="4" onclick="click_rate(this,` + data[i].id + `)" />
     <label class = "full" for="star4-${i}" title="Pretty good - 4 stars"></label>
     
-    <input type="radio" id="star3half-${i}" name="rating" value="3.5" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star3half-${i}" name="rating" value="3.5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class="half" for="star3half-${i}" title="Meh - 3.5 stars"></label>
     
-    <input type="radio" id="star3-${i}" name="rating" value="3" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star3-${i}" name="rating" value="3" onclick="click_rate(this,` + data[i].id + `)" />
     <label class = "full" for="star3-${i}" title="Meh - 3 stars"></label>
     
-    <input type="radio" id="star2half-${i}" name="rating" value="2.5" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star2half-${i}" name="rating" value="2.5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class="half" for="star2half-${i}" title="Kinda bad - 2.5 stars"></label>
     
-    <input type="radio" id="star2-${i}" name="rating" value="2" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star2-${i}" name="rating" value="2" onclick="click_rate(this,` + data[i].id + `)" />
     <label class = "full" for="star2-${i}" title="Kinda bad - 2 stars"></label>
     
-    <input type="radio" id="star1half-${i}" name="rating" value="1.5" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star1half-${i}" name="rating" value="1.5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class="half" for="star1half-${i}" title="Meh - 1.5 stars"></label>
     
-    <input type="radio" id="star1-${i}" name="rating" value="1" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star1-${i}" name="rating" value="1" onclick="click_rate(this,` + data[i].id + `)" />
     <label class = "full" for="star1-${i}" title="Sucks big time - 1 star"></label>
     
-    <input type="radio" id="star0half-${i}" name="rating" value="0.5" onclick="click_rate(this,`+data[i].id+`)" />
+    <input type="radio" id="star0half-${i}" name="rating" value="0.5" onclick="click_rate(this,` + data[i].id + `)" />
     <label class="half" for="star0half-${i}" title="Sucks big time - 0.5 stars"></label>
-</div></form>`+
-    '              </div>\n';
-        
+</div></form>` +
+                    '              </div>\n';
 
-    // content += '<div>'+data[i].name+'</div>'+
-    //  '<div>'+data[i].type.name+'</div>' ;
-}
-    document.getElementById("bookList").innerHTML = content;
-    for(let i=0;i<data.length;i++){
-        calcRate(data[i].rate,i);
-    }
-}
-});
+
+                // content += '<div>'+data[i].name+'</div>'+
+                //  '<div>'+data[i].type.name+'</div>' ;
+            }
+            document.getElementById("bookList").innerHTML = content;
+            for (let i = 0; i < data.length; i++) {
+                calcRate(data[i].rate, i);
+            }
+        }
+    });
 }
 
 function viewBook(a) {
@@ -382,7 +389,7 @@ function editBook(a) {
         data: JSON.stringify(newBook),
         type: "PUT",
         url: "http://localhost:8087/books/" + id,
-        success: function (){
+        success: function () {
             alert("sadsadsa")
         }
     })
